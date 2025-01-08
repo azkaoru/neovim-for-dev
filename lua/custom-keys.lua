de91ddb25e41df93f308e77f053874b2f15bf8d4
kaoru.watanabe <kaoru.watanabe.xn@hco.ntt.co.jp>
2025-01-09 08:36:16 +0900

    update

--- lua/custom-keys.lua
+++ lua/custom-keys.lua
@@ -13,6 +13,8 @@ map('n', '<leader>fb', ':lua require"telescope.builtin".buffers()<CR>', {})
 map('n', '<leader>ff', ':lua require"telescope.builtin".find_files({no_ignore=true, hidden=false})<CR>', {})
 -- ripgrep files, respects gitignore
 map('n', '<leader>fg', ':lua require"telescope.builtin".live_grep()<CR>', {})
+map('n', '<leader>fd', ':lua require"telescope.builtin".lsp_definitions()<CR>', {})
+map('n', '<leader>fr', '<cmd>Telescope lsp_references<CR>', {})
 
 
 -- nvim-dap keymappings
